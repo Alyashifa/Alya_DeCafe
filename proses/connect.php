63760e76be5c2aceadde5f9e591fbb380b2458e2
Alyashifa <125378295+Alyashifa@users.noreply.github.com>
2023-10-29 21:55:07 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "u837952942_alya", "/78ki$Fq9", "u837952942_alya");
+$conn = mysqli_connect("localhost", "u837952942_alya", "8?Y*[qTomJ", "u837952942_alya");
 if (!$conn){
  echo "Gagal koneksi";
 }

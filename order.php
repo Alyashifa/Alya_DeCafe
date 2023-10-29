b25e20ff458350db7d23c2ea87ee061c4ea9bf99
alyashifa <allyashfaa2004@gmail.com>
2023-10-29 13:12:51 +0700

    first commit

---
+++ order.php
@@ -0,0 +1,14 @@
+<div class="col-lg-9  mt-2">
+                <div class="card">
+                    <div class="card-header">
+                        order
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">ini adalah bagian order</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content. 
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non totam ullam doloremque magnam odit ad recusandae illum consectetur vitae, eos veritatis reiciendis quidem sequi? Nemo dignissimos rerum sint et odit.
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
